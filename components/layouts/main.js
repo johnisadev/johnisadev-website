cd8dfc1750eefbba582c209927472720380ac1b4
John Knowles <johnknowles@Johns-MacBook-Air.local>
2022-03-16 18:25:26 -0400

    created responsive navbar

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import { Box, Container } from '@chakra-ui/react'
+import Navbar from '../navbar'
 
 const Main = ({ children, router }) => {
   return (
@@ -9,6 +10,7 @@ const Main = ({ children, router }) => {
         <title>John Knowles - Portfolio</title>
       </Head>
 
+      <Navbar path={router.asPath} />
       <Container maxW="container.md" pt={14}>
         {children}
       </Container>
